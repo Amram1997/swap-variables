6e1af60a776c24e42969289b8a19cdbf1e02b964
Amram1997 <amramananyan9@gmail.com>
2021-12-01 18:51:31 +0400

    task 3 end

--- script.js
+++ script.js
@@ -2,12 +2,21 @@
 
 let i = 10;
 let n = i++ % 5;
-console.log(i,n)
+ console.log(i,n)
 // output i == 11 , output n == 0
 
 // swap two variables 
 let a = 10;
 let b = 3;
 [a,b] = [b,a]
-console.log(a,b)
+ console.log(a,b)
 // a = 3 b = 10
+
+// swap two variables without using the third one
+let x = 10;
+let y = 3;
+let swap ;
+swap = x;
+x = y;
+y = swap;
+console.log(x,y)
